13a277effaec318a9a2a4d5876a4f591f5381336
dingrui <1652959004@qq.com>
2023-01-10 16:35:24 +0800

    dev

---
+++ README.md
@@ -0,0 +1,6 @@
+# STC89C52RC
+
+## upload
+```shell
+stcgal  -P stc89 -p /dev/cu.wchusbserial14340 ./.pio/build/STC89C52RC/firmware.hex
+```
--- src/main.c
+++ src/main.c
@@ -3,14 +3,36 @@
 // P2.0管脚
 #define LED1 P2_0
 
+// led
+void light_on();
+
+void light_off();
+
+// 时延
+void delay();
+
 int main()
 {
     for (;;)
     {
-        LED1 = 0; // 点亮
-        for (int i = 0; i < 3000; i++);
-        LED1 = 1; // 熄灭
-        for (int i = 0; i < 3000; i++);
+        light_on();
+        delay();
+        light_off();
+        delay();
     }
-    return 0;
+}
+
+void light_on()
+{
+    LED1 = 0; // 点亮
+}
+
+void light_off()
+{
+    LED1 = 1; // 熄灭
+}
+
+void delay()
+{
+    for (int i = 0; i < 3000; i++);
 }
